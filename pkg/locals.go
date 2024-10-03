f63c9b2cecbdd4c2538fd11fb8d3e1a03ab17fc1
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-03 19:25:02 +0530

    setting name as id when id is not provided in input

--- pkg/locals.go
+++ pkg/locals.go
@@ -15,14 +15,22 @@ type Locals struct {
 func initializeLocals(ctx *pulumi.Context, stackInput *gcpartifactregistryv1.GcpArtifactRegistryStackInput) *Locals {
 	locals := &Locals{}
 
+	//if the id is empty, use name as id
+	if stackInput.Target.Metadata.Id == "" {
+		stackInput.Target.Metadata.Id = stackInput.Target.Metadata.Name
+	}
+
 	//assign value for the locals variable to make it available across the project
 	locals.GcpArtifactRegistry = stackInput.Target
 
 	locals.GcpLabels = map[string]string{
 		gcplabelkeys.Resource:     strconv.FormatBool(true),
-		gcplabelkeys.Organization: locals.GcpArtifactRegistry.Spec.EnvironmentInfo.OrgId,
-		gcplabelkeys.ResourceKind: "gcp_artifact_registry",
 		gcplabelkeys.ResourceId:   locals.GcpArtifactRegistry.Metadata.Id,
+		gcplabelkeys.ResourceKind: "gcp_artifact_registry",
+	}
+
+	if locals.GcpArtifactRegistry.Spec.EnvironmentInfo != nil && locals.GcpArtifactRegistry.Spec.EnvironmentInfo.OrgId != "" {
+		locals.GcpLabels[gcplabelkeys.Organization] = locals.GcpArtifactRegistry.Spec.EnvironmentInfo.OrgId
 	}
 
 	return locals
